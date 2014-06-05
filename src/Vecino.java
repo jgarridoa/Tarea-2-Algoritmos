48c9bc3f57f9b686cd519ee61834f0968b4687ec
juanjoalegria <jotaj.8a@gmail.com>
2014-06-05 17:02:25 -0400

    Se añade la clase Vecino, donde se implementa el algoritmo de búsqueda de vecino más cercano (se mantiene la clase VecinoCercano, sólo para chequear diferencias)

---
+++ src/Vecino.java
@@ -0,0 +1,57 @@
+public class Vecino {
+	private Point mejorActual;
+	private double distanciaActual;
+
+	public Vecino() {
+		mejorActual = new Point(0, 0);
+		distanciaActual = Double.MAX_VALUE;
+	}
+
+	private double distancia(Point p, Point q) {
+		return Math.sqrt(Math.pow((q.getX() - p.getX()), 2)
+				+ Math.pow((q.getY() - p.getY()), 2));
+	}
+	
+	public Point masCercano(KdTree T, Point q){
+		buscarMasCercano(T, q);
+		return mejorActual;
+	}
+
+	public void buscarMasCercano(KdTree T, Point q) {
+		if (T.isLine()) {
+			KdTree nodoCercano, nodoDistante;
+			double distanciaAlEje;
+			if (T.getPoint().getX() == 0) { // si es que estamos en una línea paralela al eje x
+				distanciaAlEje = Math.abs((T.getPoint().getY() - q.getY()));
+				if (q.getY() < T.getPoint().getY()) {
+					nodoCercano = T.izq;
+					nodoDistante = T.der;
+
+				} else {
+					nodoCercano = T.der;
+					nodoDistante = T.izq;
+				}
+
+			} else { // estamos en una línea paralela al eje y
+				distanciaAlEje = Math.abs((T.getPoint().getX() - q.getX()));
+				if (q.getX() < T.getPoint().getX()) {
+					nodoCercano = T.izq;
+					nodoDistante = T.der;
+				} else {
+					nodoCercano = T.der;
+					nodoDistante = T.izq;
+				}
+			}
+			buscarMasCercano(nodoCercano, q); // cuando esta recursión termina, se habrá encontrado el punto que está en la misma región que Q
+			if (distanciaAlEje < this.distanciaActual)
+				buscarMasCercano(nodoDistante, q);
+		} else { // si es que es un punto
+			double dist = distancia(T.getPoint(), q);
+			if (dist < distanciaActual) {
+				mejorActual = T.getPoint();
+				distanciaActual = dist;
+			}
+		}
+
+	}
+}
