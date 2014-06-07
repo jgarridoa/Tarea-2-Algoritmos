0f6af5a56000e3913f05f68e8784a6bec0a4d8a2
José <joseg.njoy@gmail.com>
2014-06-06 20:53:40 -0400

    Arreglo main y experimentos

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,10 @@ import java.io.IOException;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		//new ExperimentBuildKdTree().doMeanExperimentPrincipalMem();
-		//new ExperimentBuildKdTree().doMedianExperimentPrincipalMem();
-		new ExperimentVecinoCercano().doMeanExperiment();
+		new ExperimentBuildKdTree().doMeanExperimentPrincipalMem();
+		new ExperimentBuildKdTree().doMedianExperimentPrincipalMem();
+		new ExperimentVecinoCercano().doMeanExperimentPrincipalMem();
+		new ExperimentVecinoCercano().doMedianExperimentPrincipalMem();
 	}
 
 }
